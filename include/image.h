7be4b9ea71d149ef31f94186e35025d1c5a35da0
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2024-11-06 11:51:40 +0100

    Update image.h

--- include/image.h
+++ include/image.h
@@ -1,3 +1,10 @@
+#include "imgui.h"
+#include "backends/imgui_impl_vulkan.h"
+#include "Application.h"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
 typdef struct Image {
   void (*SetData)(struct Image *self)
   void (*resize)(struct Image *self)
@@ -5,10 +12,9 @@ typdef struct Image {
   void (*GetHeight)(struct Image *self)
   void (*AllocateMemory)(struct Image *self)
   void (*Release)(struct Image *self)
-  typedef struct ImageFormat {
+} Image;
+
+typedef enum {
     int None = 0;
     int RGBA, RGBA32F;
   } ImageFormat;
-} Image;
-
-
