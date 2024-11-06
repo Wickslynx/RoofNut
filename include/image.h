bbae69d9b0882665f4fc1e705fbcd39723f3eb81
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2024-11-06 11:46:47 +0100

    Updatet image.h.

--- include/image.h
+++ include/image.h
@@ -1,1 +1,14 @@
+typdef struct Image {
+  void (*SetData)(struct Image *self)
+  void (*resize)(struct Image *self)
+  void (*GetWidth)(struct Image *self)
+  void (*GetHeight)(struct Image *self)
+  void (*AllocateMemory)(struct Image *self)
+  void (*Release)(struct Image *self)
+  typedef struct ImageFormat {
+    int None = 0;
+    int RGBA, RGBA32F;
+  } ImageFormat;
+} Image;
+
 
