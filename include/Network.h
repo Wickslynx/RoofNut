eea6928e17ae6afc94d2cf3d422e40f27ed5cda7
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2024-11-06 12:52:04 +0100

    Added listen-

--- include/Network.h
+++ include/Network.h
@@ -3,7 +3,8 @@
 #include <stdbool.h>
 
 typedef struct RoofNutServer {
-    void* (*createSocket) (struct RoofNutServer *self, const int port, const bool listening);
+    void (*createSocket) (struct RoofNutServer *self, const int port, const bool listening);
+    void (*listen)(struct RoofNutServer *self, const int socket)
 
 } RoofNutServer;
 
