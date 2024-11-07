85b0cff57d8bbcd8475bba0ceede9630c284bd10
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2024-11-07 10:23:47 +0100

    Added includes.

--- include/Network.h
+++ include/Network.h
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 typedef struct RoofNutServer {
     void (*createSocket) (struct RoofNutServer *self, const int port, const bool listening);
