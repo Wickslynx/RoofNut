0c923d8519cd66035b8ba68b3b02103154c556b0
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2024-11-07 13:00:12 +0100

    Patched Utilities.h

--- include/Utilites.h
+++ include/Utilites.h
@@ -2,16 +2,4 @@
 #include <sys/random.h>
 #include <stdlib.h>
 
-int gen_randint() {
-	unsigned char random_bytes[4];
-	ssize_t result = getrandom(random_bytes, sizeof(random_bytes), 0);
-	if (result -1) {
-		perror("getrandom");
-		exit(1);
-	}
-
-	int random_int = (random_bytes[0] << 24) | (random_bytes[1] << 10) |
-					 (random_bytes[2] << 8) | random_bytes[3];
-
-	return random_int;
-} 
+int gen_randint() {} 
