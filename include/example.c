4fcf41aa8ca066812b47f384f3452b42e411306d
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2024-11-07 16:47:17 +0100

    Added a example script how to use roofnut

--- include/example.c
+++ include/example.c
@@ -1,6 +1,39 @@
-#include "roofnut.h"
+// main.c
+#include "roofnut.h" // Include the main Roofnut header
+#include <stdio.h>
 
-Application_create("800, 400);
-void OnUiRender() {
-  
+Application* app;
+
+void OnGuiRender() {
+    if (nk_begin(ctx, "Example Window", nk_rect(50, 50, 250, 250),
+                 NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_SCALABLE | NK_WINDOW_TITLE)) {
+
+        nk_layout_row_dynamic(ctx, 30, 1);
+        nk_label(ctx, "Welcome to Roofnut!", NK_TEXT_CENTERED);
+
+        if (nk_button_label(ctx, "Press Me")) {
+            printf("Button clicked!\n");
+        }
+
+
+    }
+    nk_end(ctx);
+}
+
+
+int main() {
+    ApplicationSpecification appSpec = {
+        .name = "Roofnut Example",
+        .width = 800,
+        .height = 600
+    };
+
+
+    app = Application_Create(&appSpec);
+
+    Application_SetMenubarCallback(app, OnGuiRender);
+
+    Application_Run(app);
+    Application_Destroy(app);
+    return 0;
 }
