04555f335af07114f7e74380b8cd66e84fb8ad33
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2024-11-07 16:54:30 +0100

    Added comments and more. 1.11

--- include/example.c
+++ include/example.c
@@ -2,9 +2,9 @@
 #include "roofnut.h" // Include the main Roofnut header
 #include <stdio.h>
 
-Application* app;
+Application* app; //Init application.
 
-void OnGuiRender() {
+void OnGuiRender() { //Write your Nuclear code here. (Will be run every frame.)
     if (nk_begin(ctx, "Example Window", nk_rect(50, 50, 250, 250),
                  NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_SCALABLE | NK_WINDOW_TITLE)) {
 
@@ -22,18 +22,18 @@ void OnGuiRender() {
 
 
 int main() {
-    ApplicationSpecification appSpec = {
+    ApplicationSpecification appSpec = { //Set the application specification.
         .name = "Roofnut Example",
         .width = 800,
         .height = 600
     };
 
 
-    app = Application_Create(&appSpec);
+    app = Application_Create(&appSpec); //Create application.
 
-    Application_SetMenubarCallback(app, OnGuiRender);
+    Application_SetMenubarCallback(app, OnGuiRender); //Set callback function for Ui rendering.
 
-    Application_Run(app);
-    Application_Destroy(app);
+    Application_Run(app); //Run the application.
+    Application_Destroy(app); //Destroy the application when done.
     return 0;
 }
