5004ed17aab89b51e5e260efa9ef00320b55c9d8
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2024-11-09 08:53:00 +0100

    Test Phase 1.

--- include/input.h
+++ include/input.h
@@ -3,7 +3,8 @@
 
 
 void Input_Init(GLFWwindow* window);
-
+void keycallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+void cursor_position_callback(GLFWwindow* window, double xpos, double ypos);
 int isKeyPressed(int button);
 
 int isMouseButtonPressed(int button);
