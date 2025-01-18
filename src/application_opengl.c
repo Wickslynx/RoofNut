084ebc117bf247a09256595f0ee4b5498eedbf96
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-01-18 14:11:05 +0100

    Change Glew versiom from 3.3 to 3.0 :)

--- src/application_opengl.c
+++ src/application_opengl.c
@@ -83,7 +83,7 @@ void init_opengl() {
 
     // Set GLFW window hints before window creation
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     #ifdef __APPLE__
         glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
