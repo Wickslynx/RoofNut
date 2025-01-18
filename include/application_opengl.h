66795978c17ab0511d6d4198b8a49b683b1bb9ba
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-01-18 14:33:39 +0100

    Add something..

--- include/application_opengl.h
+++ include/application_opengl.h
@@ -4,6 +4,22 @@
 #include <stdbool.h>   // Include <stdbool.h> for bool type
 #include <string.h>
 
+#include <GL/glew.h>
+#include "external/glfw/include/GLFW/glfw3.h"
+
+
+//Define and set the GLFW window to null. 
+extern GLFWwindow *g_Window; 
+GLFWwindow* g_Window = NULL;
+
+
+struct Application {
+    struct ApplicationSpecification specification;
+    bool running;
+    bool customTitleBar;
+    GLFWwindow* windowHandle;
+};
+
 
 void init_opengl(void);
 void RoofNut_loop(void);
