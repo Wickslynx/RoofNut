d0f7355b96cdac177eb4faf5d7653f5b26ab4b34
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-01-25 20:52:27 +0100

    Update image.h

--- include/image.h
+++ include/image.h
@@ -1,21 +1,34 @@
-
-#include "librarys/Nuklear/nuklear_glfw_vulkan.h"
 #include "application.h"
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-typdef struct Image {
-  void (*SetData)(struct Image *self)
-  void (*resize)(struct Image *self)
-  void (*GetWidth)(struct Image *self)
-  void (*GetHeight)(struct Image *self)
-  void (*AllocateMemory)(struct Image *self)
-  void (*Release)(struct Image *self)
+// Define Image and ImageFormat structs
+typedef enum {
+    IMAGE_FORMAT_RGBA,
+    IMAGE_FORMAT_RGBA32F
+} ImageFormat;
+
+typedef struct {
+    char* filepath;
+    int width;
+    int height;
+    ImageFormat format;
+    GLuint textureID;
 } Image;
 
-typedef enum {
-    int None = 0;
-    int RGBA, RGBA32F;
-  } ImageFormat;
-//Note: This is broken, i don't understand what this does as it is AI written. Its scrapped.
+
+// Utility functions
+static uint32_t Utils_BytesPerPixel(ImageFormat format);
+static GLuint Utils_LoadTexture(const char* filepath, int* width, int* height, ImageFormat* format);
+Image* Image_Create(const char* path);
+void Image_Destroy(Image* img);
+void Image_Render(Image* img, float x, float y, float width, float height);
+
+static uint32_t Utils_BytesPerPixel(ImageFormat format) {
+    switch (format) {
+        case IMAGE_FORMAT_RGBA:    return 4;
+        case IMAGE_FORMAT_RGBA32F: return 16;
+        default:                   return 0;
+    }
+}
