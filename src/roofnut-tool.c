5f188216c788cf662027177fbe76515716b46535
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-01-25 19:45:32 +0100

    Polished up the version a bit.

--- src/roofnut-tool.c
+++ src/roofnut-tool.c
@@ -5,7 +5,7 @@
 int main(int argc, char *argv[]) {
     if (argc > 1) {
         if (strcmp(argv[1], "--version") == 0) {
-            printf("RoofNut v1.1.2 \n(L) MIT license. \nSupport: Wickslynx@yahoo.com\n");
+            printf("(V) RoofNut v1.1.2 \n(L) MIT license. \nBuild: Stable-release. \nSupport: Wickslynx@yahoo.com\n");
         }
     } else {
         printf("Please specify a command [--version --force-update --force-quit --logs]\n");
