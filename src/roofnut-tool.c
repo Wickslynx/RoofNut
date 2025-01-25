89bb6f5adccc7ad893acd5642a0cbba799529e14
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-01-25 19:36:05 +0100

    Create roofnut-tool.c

---
+++ src/roofnut-tool.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    if (argc > 1) {
+        if (strcmp(argv[1], "--version") == 0) {
+            printf("RoofNut v1.1.2 \n (L) MIT license. \n Support: Wickslynx@yahoo.com\n");
+        }
+    } else {
+        printf("Please specify a command [--version --force-update --force-quit --logs]\n");
+    }
+    return 0;
