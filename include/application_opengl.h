a77bcba0571f1cf25c06041ae1ce68de331170c8
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-01-25 16:27:44 +0100

    Update application_opengl.h

--- include/application_opengl.h
+++ include/application_opengl.h
@@ -22,11 +22,7 @@ struct ApplicationSpecification {
 void init_opengl(struct Application* app);
 void RoofNut_loop(struct Application* app);
 void DestroyOpenGl(void);
-void RoofNutRender() {
-    #include "external/Nuklear/nuklear.h"
-    #include "external/Nuklear/nuklear_glfw_gl3.h"
-    struct nk_context *ctx; 
-}
+void RoofNutRender();
 
 
 
