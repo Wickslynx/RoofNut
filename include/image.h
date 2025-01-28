ebbd66aa0ad7337d9aa07f2a1471b4d5f58abe73
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-01-28 08:10:19 +0100

    Tried to fix error.

--- include/image.h
+++ include/image.h
@@ -1,9 +1,6 @@
 
 #include <GL/gl.h>
 
-#define STB_IMAGE_IMPLEMENTATION
-#include "stb_image.h"
-
 // Define Image and ImageFormat structs
 
 unsigned char* loadImage(const char* filename, int* width, int* height, int* channels);
