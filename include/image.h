52cda072a934f0213f8a12a5f35b4f464678c9b2
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-01-29 18:03:47 +0100

    Updated header :D

--- include/image.h
+++ include/image.h
@@ -1,8 +1,19 @@
+#ifndef IMAGE_H
+#define IMAGE_H
 
-#include <GL/gl.h>
+#include <GL/glew.h>
 
-// Define Image and ImageFormat structs
+typedef struct {
+    GLuint textureID;
+    GLuint VAO;
+    GLuint VBO;
+    GLuint EBO;
+    GLuint shaderProgram;
+} ImageRenderer;
 
 unsigned char* loadImage(const char* filename, int* width, int* height, int* channels);
-GLuint createTexture(const char* filename);
-void RenderImage(const char* imagePath);
+ImageRenderer* createImageRenderer(const char* filename);
+void renderImage(ImageRenderer* renderer);
+void destroyImageRenderer(ImageRenderer* renderer);
+
+#endif
