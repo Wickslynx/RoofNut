3e6e49caf9cc4bdd3dbf110c5a2be10199a28139
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-01-31 06:49:36 +0100

    Fixed header.

--- include/application_opengl.h
+++ include/application_opengl.h
@@ -1,32 +1,53 @@
+#ifndef APPLICATION_OPENGL_H
+#define APPLICATION_OPENGL_H
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdbool.h>   
-#include <string.h>
+#include <stdbool.h>
+#include "external/glew/include/GL/glew.h"
+#include "external/glfw/include/GLFW/glfw3.h"
 
+// Forward declarations:
+struct nk_context;
+struct nk_font_atlas;
+struct nk_glfw;
 
 #define MAX_VERTEX_MEMORY 512 * 1024
 
 #define MAX_ELEMENT_MEMORY 128 * 1024
 
-struct Application;
 
 struct ApplicationSpecification {
+    int width;
+    int height;
     const char* name;
-    int width; 
-    int height; 
 };
 
 
 
-void init_opengl(struct Application* app);
-void RoofNutLoop(struct Application* app);
-void limit_frame_rate(struct Application* app);
-void DestroyOpenGl(void);
-void RoofNutRender();
+struct Application {
+    struct ApplicationSpecification specification;
+    bool running;
+    bool customTitleBar;
+    GLFWwindow* windowHandle;
+    struct {
+        double lastFrameTime;
+        double targetFrameRate;
+        bool vsyncEnabled;
+    } timing;
+    struct {
+        struct nk_context *ctx;
+        struct nk_font_atlas *atlas;
+        struct nk_glfw *glfw;  
+    } nuklear;
+};
 
 
-
-
+// Function declarations:
 struct Application* RoofNutCreate(const struct ApplicationSpecification* specification);
 void RoofNutDestroy(struct Application* app);
+void RoofNutGetWindowSize(int* width, int* height);
+void RoofNutWindowShouldClose(void);
+void RoofNutRender();
+void DestroyOpenGL(struct Application* app);
+void limit_frame_rate(struct Application* app);
+
+#endif // APPLICATION_OPENGL_H
