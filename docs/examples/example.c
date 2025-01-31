f7abc8a73753c2a13bca8a3ca427f81eca48e487
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-01-31 12:58:32 +0100

    Fixed example.

--- docs/examples/example.c
+++ docs/examples/example.c
@@ -1,29 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define ROOFNUT_IMPLEMENTATION //If not defined, roofnut wont be able to be used.
+#define ROOFNUT_DEBUG //Set roofnut to debug mode.
+//ROOFNUT_USE_VULKAN (Restrict it to only Vulkan)
+//ROOFNUT_USE_OPENGL (Restrict it to only OpenGL)
+
 #include <roofnut.h>
 
-//Debug = On
-#define ROOFNUT_DEBUG  
-
-//RoofNut will automaticly detect the right lib.
-//Use OpenGL -- If you want to restrict it to a single lib only.
-//#define ROOFNUT_USE_OPENGL 
-//Uncomment to use vulkan. -- If you want to restrict it to a single lib only.
-//#define ROOFNUT_USE_VULKAN
   
-#define ROOFNUT_IMPLEMENTAON
 //Set this to your applications name.
-  
 #define APPLICATION_NAME "EXAMPLE"
 
 extern struct nk_context *ctx; 
 
 void RoofNutRender() {
-    //Insert your Nuklear code here.
+  //Insert your Nuklear code here.
   if (nk_begin(ctx, "", nk_rect(50, 50, 1280, 720),
                      NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_SCALABLE |
                      NK_WINDOW_MINIMIZABLE | NK_WINDOW_TITLE)) {
   }
-  RenderImage("path-to-image");
+  RenderImage("path-to-image", 100, 100, 100, 100);
   nk_end(ctx);
 
   
@@ -38,12 +36,12 @@ int main() {
     };
 
     // Create Application
-    struct Application* app = Application_Create(&appSpec);
+    struct Application* app = RoofNutCreate(&appSpec);
     if (!app) {
         fprintf(stderr, "Failed to create application!\n");
         return EXIT_FAILURE;
     }
     
-    Application_Destroy(app);
+    RoofNutDestroy(app);
     return 0;
 }
