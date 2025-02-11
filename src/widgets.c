4e642c7e479a14f47f45722e4858e626e301e46d
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-02-11 11:21:26 +0100

    Update widgets.c

--- src/widgets.c
+++ src/widgets.c
@@ -5,6 +5,84 @@
 #include "widgets.h"
 
 
+#ifdef ROOFNUT_USE_VULKAN
 
 
-#endif
+
+#else //ROOFNUT_USE_OPENGL
+
+#include "external/glew/include/GL/glew.h"
+#include "external/glfw/include/GLFW/glfw3.h"
+
+#include "widgets.h"
+
+#include <math.h>
+
+
+GLuint compileShader(GLenum type, const char* source) {
+    GLuint shader = glCreateShader(type);
+    glShaderSource(shader, 1, &source, NULL);
+    glCompileShader(shader);
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        fprintf(stderr, "ERROR::SHADER::COMPILATION_FAILED\n%s\n", infoLog);
+    }
+    return shader;
+
+void RoofNutTriangle(float x1, float y1, float x2, float y2, float x3, float y3) {
+    float vertices[] = {
+        x1, y1, 0.0f,
+        x2, y2, 0.0f,
+        x3, y3, 0.0f
+    };
+
+    GLuint VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    GLuint vertexShader = compileShader(GL_VERTEX_SHADER, vertexShaderSource);
+    GLuint fragmentShader = compileShader(GL_FRAGMENT_SHADER, fragmentShaderSource);
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    int success;
+    char infoLog[512];
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        fprintf(stderr, "ERROR::SHADER::PROGRAM::LINKING_FAILED\n%s\n", infoLog);
+    }
+    glUseProgram(shaderProgram);
+
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glBindVertexArray(0);
+    glDeleteBuffers(1, &VBO);
+    glDeleteVertexArrays(1, &VAO);
+}
+
+
+
+
+
+
+#endif //ROOFNUT_USE_VULKAN & ROOFNUT_USE_OPENGL
+
+
+#endif //ROOFNUT_INCLUDE_INTERNAL_WIDGETS
