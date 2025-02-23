68c5fce5a66ff46317233ed829172257f8063a3c
Wickslynx <167521638+Wickslynx@users.noreply.github.com>
2025-02-23 14:28:11 +0100

    ADWa

--- include/widgets.h
+++ include/widgets.h
@@ -2,6 +2,8 @@
 #define WIDGETS_H
 
 #ifdef ROOFNUT_USE_OPENGL
+
+void RoofNutTriangle(float x1, float y1, float x2, float y2, float x3, float y3);
 //Shaders and stuff.
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
